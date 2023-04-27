95ef1fe4f983a5f9c27c5716f46ade2708c9a181
Jorge de Souza <jorgejrdj1@gmail.com>
2023-04-27 19:19:41 -0300

    feat: using knex queries

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import fastify from 'fastify'
 import { knex } from './database'
-
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-  return tables
+  return transactions
 })
 
 app
