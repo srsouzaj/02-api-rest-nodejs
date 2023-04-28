b5cdef98d355752f97fbbcadba1180ba9455e8e5
Jorge de Souza <jorgejrdj1@gmail.com>
2023-04-28 20:53:09 -0300

    feat: validate env

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
+
 const app = fastify()
 
 app.get('/hello', async () => {
   const transactions = await knex('transactions')
     .where('amount', 1000)
     .select('*')
-
   return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
