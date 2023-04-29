b2b0a7783a6053e1a769610bcbe23c132ac448aa
Jorge de Souza <jorgejrdj1@gmail.com>
2023-04-28 22:38:31 -0300

    feat: add types knex

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -4,13 +4,13 @@ import { Knex } from 'knex'
 // import 'knex'
 
 declare module 'knex/types/tables' {
-    export interface Tables {
-        transactions: {
-            id: string
-            title: string
-            amount: number
-            created_at: string
-            session_id?: string
-        }
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
     }
+  }
 }
