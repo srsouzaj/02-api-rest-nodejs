a08fd8612dadf2bac4d3de4d6989760b70af734f
Jorge de Souza <jorgejrdj1@gmail.com>
2023-04-29 00:25:25 -0300

    feat: organize aplicarion for deploy

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,14 @@
-import 'dotenv/config'
 import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL env not found.')
-}
-
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+        filename: env.DATABASE_URL,
+      }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,5 @@
 import { config } from 'dotenv'
 import { z } from 'zod'
-
 if (process.env.NODE_ENV === 'test') {
   config({ path: '.env.test' })
 } else {
@@ -9,9 +8,11 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
+
 const _env = envSchema.safeParse(process.env)
 if (_env.success === false) {
   console.error('⚠️ Invalid environment variables', _env.error.format())
