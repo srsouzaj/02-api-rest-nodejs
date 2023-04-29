01209b41a69914a1ab40970449e397f8f1037e7c
Jorge de Souza <jorgejrdj1@gmail.com>
2023-04-28 22:40:07 -0300

    feat: using cookies fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,15 +2,18 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
+
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select()
     return { transactions }
   })
+
   app.get('/:id', async (request) => {
     const getTransactionsParamsSchema = z.object({
       id: z.string().uuid(),
     })
+
     const { id } = getTransactionsParamsSchema.parse(request.params)
     const transaction = await knex('transactions').where('id', id).first()
     return {
@@ -22,7 +25,6 @@ export async function transactionsRoutes(app: FastifyInstance) {
     const summary = await knex('transactions')
       .sum('amount', { as: 'amount' })
       .first()
-
     return { summary }
   })
 
@@ -35,11 +37,25 @@ export async function transactionsRoutes(app: FastifyInstance) {
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
+
     return reply.status(201).send()
   })
 }
