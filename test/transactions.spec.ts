febfa34a247e806abc23fe3a7771868e7189d1ca
Jorge de Souza <jorgejrdj1@gmail.com>
2023-04-29 00:17:01 -0300

    feat: config test db

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -9,6 +10,12 @@ describe('Transactions routes', () => {
     afterAll(async () => {
         await app.close()
     })
+
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all')
+        execSync('npm run knex migrate:latest')
+    })
+
     it('should be able to create a new transaction', async () => {
         await request(app.server)
             .post('/transactions')
@@ -19,7 +26,6 @@ describe('Transactions routes', () => {
             })
             .expect(201)
     })
-
     it('should be able to list all transactions', async () => {
         const createTransactionResponse = await request(app.server)
             .post('/transactions')
@@ -28,14 +34,11 @@ describe('Transactions routes', () => {
                 amount: 5000,
                 type: 'credit',
             })
-
         const cookies = createTransactionResponse.get('Set-Cookie')
-
         const listTransactionsResponse = await request(app.server)
             .get('/transactions')
             .set('Cookie', cookies)
             .expect(200)
-
         expect(listTransactionsResponse.body.transactions).toEqual([
             expect.objectContaining({
                 title: 'New transaction',
